d66eb187f8fe966a1b94a595b3bd8dcb507852cd
Chantuu <47988187+Chantuu@users.noreply.github.com>
2023-07-12 02:24:41 +0400

    Add boileplate code for calculator app

---
+++ app.js
@@ -0,0 +1,7 @@
+const screenOperationSpan = document.querySelector('.number.operation');
+const screenResultSpan = document.querySelector('.number.result');
+
+const calculatorButtonNodeList = document.querySelectorAll('button');
+calculatorButtonNodeList.forEach(button => button.addEventListener('click', e => {
+    
+}));
