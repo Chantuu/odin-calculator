1619a8822a5a58ea9cac5256cc125589a820072b
Chantuu <47988187+Chantuu@users.noreply.github.com>
2023-07-12 04:08:49 +0400

    Add logic to save operands and display them

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const screenOperationSpan = document.querySelector('.number.operation');
 const screenResultSpan = document.querySelector('.number.result');
 
-const calculatorButtonNodeList = document.querySelectorAll('button');
-calculatorButtonNodeList.forEach(button => button.addEventListener('click', e => {
-    
+let firstOperand = '';
+let operator = '';
+let secondOperand = '';
+
+const numberButtons = document.querySelectorAll('.button.number');
+numberButtons.forEach(number => number.addEventListener('click', e => {
+    const buttonNode = e.target;
+
+    if (!operator) {
+        firstOperand += buttonNode.innerText;
+        screenOperationSpan.innerText = firstOperand;
+    } else {
+        secondOperand = buttonNode.innerText;
+        screenOperationSpan.innerText += secondOperand;
+    }
 }));
+
+const operatorButtons = document.querySelectorAll('.button.operator');
+operatorButtons.forEach(number => number.addEventListener('click', e => {
+    const buttonNode = e.target;
+
+    if (operator === '') {
+        operator = buttonNode.innerText;
+        screenOperationSpan.innerText += operator;
+    }
+}));
+
