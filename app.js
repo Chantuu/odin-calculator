b4c5acd2230bd7ddfd44be4fd56556ca438483c0
Chantuu <47988187+Chantuu@users.noreply.github.com>
2023-07-17 02:02:40 +0400

    Add code comments
     Documented code with comments for code logic explanation

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+/* Calculator operator screen spans*/
 const firstOperandSpan = document.querySelector('.number.operation.first');
 const secondOperandSpan = document.querySelector('.number.operation.second');
 const thirdOperandSpan = document.querySelector('.number.operation.third');
+
+/* Calculator answer (result) screen span */
 const screenResultSpan = document.querySelector('.number.result');
 
 let firstOperand = '';
@@ -15,6 +18,7 @@ function operateCleanup(result) {
 }
 
 function operate() {
+    /* Checks for digit after . in second operand and automatically adds 0 if no digit is present after .*/
     if ((secondOperand.lastIndexOf('.') + 1) === secondOperand.length && secondOperand.length > 0) {
         secondOperand += '0';
         thirdOperandSpan.innerText += '0';
@@ -37,7 +41,7 @@ function operate() {
 
 const numberButtons = document.querySelectorAll('.button.number');
 numberButtons.forEach(number => number.addEventListener('click', e => {
-    const buttonNode = e.target;
+    const buttonNode = e.target; /* Gets button element from event */
 
     if (!operator) {
         firstOperand += buttonNode.innerText;
@@ -50,19 +54,22 @@ numberButtons.forEach(number => number.addEventListener('click', e => {
 
 const operatorButtons = document.querySelectorAll('.button.operator');
 operatorButtons.forEach(number => number.addEventListener('click', e => {
-    const buttonNode = e.target;
+    const buttonNode = e.target; /* Gets button element from event */
     
+    /* Checks for digit after . in first operand and automatically adds 0 if no digit is present after .*/
     if ((firstOperand.lastIndexOf('.') + 1) === firstOperand.length && firstOperand.length > 0) {
         firstOperand += '0';
         firstOperandSpan.innerText += '0';
     }
 
+    /* Saves calculation result as first operand if user clicks on operator buttons*/
     if (screenResultSpan.innerText !== '') {
         firstOperandSpan.innerText = screenResultSpan.innerText;
         screenResultSpan.innerText = '';
         thirdOperandSpan.innerText = '';
     }
 
+    /* Adds operator to calculation screen span */
     if (operator === '') {
         operator = buttonNode.innerText;
         secondOperandSpan.innerText = operator;
@@ -71,14 +78,22 @@ operatorButtons.forEach(number => number.addEventListener('click', e => {
 
 const dotButton = document.querySelector('.button.dot');
 dotButton.addEventListener('click', () => {
+    /* Adds . for first operand */
     if (operator === '') {
+
+        /* Checks for digit before . and adds 0 if no digit is present after . Else . is added to number*/
         if (!firstOperand.includes('.') && firstOperand === '') {
             firstOperand = '0.';
         } else if (!firstOperand.includes('.')) {
             firstOperand += '.';
         }
         firstOperandSpan.innerText = firstOperand;
-    } else {
+    } 
+    
+    /* Adds . for second operand */
+    else {
+
+        /* Checks for digit before . and adds 0 if no digit is present after . Else . is added to number*/
         console.log(secondOperand.includes('.'));
         if (!secondOperand.includes('.') && secondOperand === '') {
             secondOperand = '0.';
@@ -94,34 +109,52 @@ evaluatorButton.addEventListener('click', operate);
 
 const topButtons = document.querySelectorAll('.button.control');
 topButtons.forEach(button => button.addEventListener('click', e => {
-    const buttonElement = e.target;
+    const buttonElement = e.target; /* Gets button element from event */
 
+    /* Clear button functionality */
     if (buttonElement.innerText === 'Clear') {
+        
+        /* Clears calculator variables */
         firstOperand = '';
         operator = '';
         secondOperand = '';
 
+        /* Clears calculator operation screen spans */
         firstOperandSpan.innerText = '';
         secondOperandSpan.innerText = '';
         thirdOperandSpan.innerText = '';
 
+        /* Clears calculator result screen span */
         screenResultSpan.innerText = '';
-    } else if (buttonElement.innerText === 'Delete') {
+    } 
+    
+    /* Delete button functionality */
+    else if (buttonElement.innerText === 'Delete') {
+
+        /* Removes digits from second operand */
         if (firstOperand !== '' && operator !== '' && secondOperand !== '') {
             secondOperand = secondOperand.slice(0, secondOperand.length - 1);
             thirdOperandSpan.innerText = secondOperand;
 
+            /* Checks if one digit is left in operand*/
             if (secondOperand.length === 0) {
                 secondOperand = '';
                 thirdOperandSpan.innerText = secondOperand;
             }
-        } else if (firstOperand !== '' && operator !== '' && secondOperand === '') {
+        } 
+        
+        /* Removes operator */
+        else if (firstOperand !== '' && operator !== '' && secondOperand === '') {
             operator = '';
             secondOperandSpan.innerText = operator;
-        } else if (firstOperand !== '' && operator === '' && secondOperand === '') {
+        } 
+        
+        /* Removes digits from first operand */
+        else if (firstOperand !== '' && operator === '' && secondOperand === '') {
             firstOperand = firstOperand.slice(0, firstOperand.length - 1);
             firstOperandSpan.innerText = firstOperand;
 
+            /* Checks if one digit is left in operand*/
             if (firstOperand.length === 0) {
                 firstOperand = '';
                 firstOperandSpan.innerText = firstOperand;
